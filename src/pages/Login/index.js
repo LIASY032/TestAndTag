59174e8488a6eafc66dc3dd400a67bc457c5d3df
LIASY032 <66517361+LIASY032@users.noreply.github.com>
2022-04-10 19:53:27 +0930

    react router dom and Login page added

---
+++ src/pages/Login/index.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+import { Form, Button } from "react-bootstrap";
+function Login() {
+  return (
+    <>
+      <Form
+        style={{
+          width: "30%",
+          border: "1px solid #8f8e8e",
+          marginTop: "10%",
+          marginLeft: "auto",
+          marginRight: "auto",
+        }}
+      >
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </>
+  );
+}
+
+export default Login;

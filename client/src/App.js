268ef4dce4f0826f2c8f91662ae90ec1787d1d79
LIASY032 <66517361+LIASY032@users.noreply.github.com>
2022-04-19 22:26:44 +0930

    move to client folder

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navigation from "./components/Navigation";
+import Login from "./pages/Login";
+import Registration from "./pages/Registration";
+import Forgot from "./pages/Forgot";
+import Request from "./pages/Request";
+import { AuthorisedDashboard, UserDashboard } from "./pages/Dashboard";
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navigation />
+
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Registration />} />
+          <Route path="/forgot" element={<Forgot />} />
+          <Route path="/request" element={<Request />} />
+          <Route path="/user" element={<UserDashboard />} />
+          <Route path="/tester" element={<AuthorisedDashboard />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Navigation/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { PersonCircle } from "react-bootstrap-icons";
+function Navigation() {
+  return (
+    <Navbar style={{ backgroundColor: "#8f8e8e" }} expand="lg">
+      <Container style={{ color: "white" }}>
+        <Navbar.Brand href="/">Test And Tag</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/login">Login</Nav.Link>
+            <Nav.Link href="/request">Request</Nav.Link>
+
+            <Nav.Link href="/user">User</Nav.Link>
+            <Nav.Link href="/tester">Authorised</Nav.Link>
+            <Nav.Link>
+              <PersonCircle />
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Navigation;

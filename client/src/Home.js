0e0b827d5ca867b23e3e388222a15413a2ca1012
LiKe <keyly003@mymail.unisa.edu.au>
2022-05-23 23:40:47 +0800

    client: remove work calendar page

--- client/src/Home.js
+++ client/src/Home.js
@@ -4,11 +4,11 @@ import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import SignIn from "./pages/SignIn";
 import Dashboard from "./pages/Dashboard";
-import WorkCalendar from "./pages/WorkCalendar";
 import CreateItem from "./pages/CreateItem";
 import OldItem from "./pages/OldItem";
 import SubmitSuccess from "./pages/SubmitSuccess";
 import WorkList from "./pages/WorkList";
+import TaskPool from "./pages/TaskPool";
 import Statics from "./pages/Statics";
 import TaskDetails from "./pages/TaskDetails";
 import MenuLogin from "./components/MenuLogin";
@@ -44,12 +44,12 @@ class Home extends Component {
                     <Route path="/" element={<HomePage />} />
                     <Route path="/sign_in" element={<SignIn changeIsLogin={this.changeIsLogin} />} />
                     <Route path="/dashboard" element={<Dashboard />} />
-                    <Route path="/work_calendar" element={<WorkCalendar />} />
                     <Route path="/new" element={<CreateItem />} />
                     <Route path="/old" element={<OldItem />} />
                     <Route path="/submit_success" element={<SubmitSuccess />} />
                     <Route path="/work_list" element={<WorkList />} />
                     <Route path="/work_details" element={<TaskDetails />} />
+                    <Route path="/pool_list" element={<TaskPool />} />
                     <Route path="/statics" element={<Statics />} />
                 </Routes>
             </Router>
